b7ed189ebb9e9c3998a0b94ac60b23cf365f26ea
Marek Ozimina <marek.ozimina@yahoo.com>
2025-10-04 21:59:00 +0200

    wstepny plik re_detection.py

--- app/rem_detection.py
+++ app/rem_detection.py
@@ -1,4 +1,14 @@
 
 
-def rem_detection():
-    print("Rem detection start")
+def rem_detection()->bool:# Dodajemy tutaj wyjściowe z pozostałych funkcji z pliku oraz flagi atonia flag i sleep flag
+    
+    pass
+
+def check_medium_hr():
+    # Funkcja sprawdza średnie HR z ostatnich 15 minut
+    
+    pass
+
+def compare_medium_hr():
+    # Funkcja porównuję średnie hr do hr z ostatnich 30 sekund
+    pass
--- app/routes/embedded.py
+++ app/routes/embedded.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-#
+from ..rem_detection import rem_detection
 
 embedded_bp = Blueprint('embedded', __name__)
 
@@ -13,5 +13,15 @@ def embedded_data():
     data = request.get_json()
     # Parsujemy i analizujemy dane żeby zapisać w sesji na ich podstawie czy użytkownik śpi i jest w fazie rem
     # Zapisujemy je w sesji żeby móc odpowiedzieć na ping aplikacji mobilnej i odpowiedzieć flagą
+
+    # WARUNKI DLA N_REM:
+    # - HR - jest zwolnione
+    # - HR - jest stabilne (można sprawdzić za pomocą HRV)
+    # - HRV - Oddechy są równe
+    # - HRV - Oddechy są długie
+    # - RR - Na jego podstawie określamy czy długość oddechu jest dłuższa niż normalnie
+
+    # WARUNKI DO ZMIERZENIA 
+    # Sprawdzamy średni HR z ostatnich 15 minut - jeśli w ciągu ostatniej pół minuty wzrosło o daną wartość - flaga rem true
     print(data)
     return jsonify({"status": "success","device_id": "Dev_001", "timestamp": "2025-09-24T23:45:12Z"})
