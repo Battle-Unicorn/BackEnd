aa69e66d0521898ac2848582fa624fb790336068
Marek Ozimina <marek.ozimina@yahoo.com>
2025-10-04 20:36:43 +0200

    Poprawy w mocku. Dodanie pustego utils.py

--- app/routes/embedded.py
+++ app/routes/embedded.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+#
 
 embedded_bp = Blueprint('embedded', __name__)
 
@@ -6,3 +7,9 @@ embedded_bp = Blueprint('embedded', __name__)
 @embedded_bp.route('/embedded/hello')
 def embedded_helo():
     return jsonify("Hello from embedded")
+
+@embedded_bp.route('/embedded/data', methods=['POST'])
+def embedded_data():
+    data = request.get_json()
+    print(data)
+    return jsonify({"status": "success","device_id": "Dev_001", "timestamp": "2025-09-24T23:45:12Z"})
